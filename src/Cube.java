bf2c51c95b97a88cda5c4d79d7a7cb5d73783f4b
Jelle Hoffman <jelle.hoffman@hotmail.com>
2017-12-16 14:50:37 +0100

    Cleaning the code
    Cleaned the code so it is easier to read.

--- src/Cube.java
+++ src/Cube.java
@@ -4,18 +4,38 @@ public class Cube {
 	private int[][][] array;
 	
 	public Cube(int size){
-		array = new int[size][size][size];
+		this.size = size+1;
+		array = new int[this.size][this.size][this.size];
 	}
 	
-	public void update(int x,int y, int z, int value){
-		array[x][y][z] = value; 
+	/**
+	 * Method to assign a new value to a specific point in the cube. 
+	 * @param values A Array of 4 Integers. The first three correspond with a point in the cube.
+	 * The fourth is the new value this point should get.
+	 * @throws ArrayIndexOutOfBoundsException if one of the values is not within the cube. 
+	 */
+	public void update(int[] values) throws ArrayIndexOutOfBoundsException{
+			array[values[0]][values[1]][values[2]] = values[3];
 	}
 	
-	public int query(int x1, int y1, int z1, int x2, int y2, int z2){
+	/**
+	 * Method to calculate the value of all the points within two points in a 3D cube. 
+	 * @param values A Array of 6 Integers that correspond to two points in the Cube
+	 * @return The sum of the values within the two points 
+	 * @throws ArrayIndexOutOfBoundsException if one of the values is not within the cube. 
+	 */
+	public int query(int[] values) throws ArrayIndexOutOfBoundsException{
+		int x1 = values[0];
+		int y1 = values[1];
+		int z1 = values[2];
+		int x2 = values[3];
+		int y2 = values[4];
+		int z2 = values[5];
+		
 		int sum = 0;
-		for (int i = x1; i < x2; i++){
-			for (int j = y1; j < y2; j++){
-				for (int k = z1; k < z2; k++){
+		for (int i = x1; i <= x2; i++){
+			for (int j = y1; j <= y2; j++){
+				for (int k = z1; k <= z2; k++){
 					sum += array[i][j][k];
 				}
 			}
--- src/main.java
+++ src/main.java
@@ -1,65 +1,99 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
-public class main {	
-	
+public class main {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		System.out.println("Hello, please fill in the size of the cube:");
 		String input = br.readLine();
 		while(!isPositiveInteger(input)){
-			System.out.println("Your input is not a positive round. Please fill in a positive round number:");
+			System.out.println("Your input is not a positive round number under 100. Please fill in a positive round number:");
 			input = br.readLine();
 		}
 		int size = Integer.parseInt(input);
 		System.out.println("Creating a cube with size: "+size);
 		Cube cube = new Cube(size);
 		System.out.println("Please fill in what you want to do. QUERY or UPDATE with appropriat values.");
+		
 		while(true){
 			input = br.readLine();
 			String[] separated_text = input.split(" ");
-			if (separated_text[0].contains("UPDATE")){
-				if(separated_text.length == 5){
-					if(isPositiveInteger(separated_text[1])&&isPositiveInteger(separated_text[2])&&isPositiveInteger(separated_text[3])&&isPositiveInteger(separated_text[4])){
-						cube.update(Integer.parseInt(separated_text[1]), Integer.parseInt(separated_text[2]), Integer.parseInt(separated_text[3]), Integer.parseInt(separated_text[4]));
-					}
-				}else{
-					System.out.println("Too much or too little values.");
-				}
-			}else if (separated_text[0].contains("QUERY")){
-				if(separated_text.length == 7){
-					if(isPositiveInteger(separated_text[1])&&isPositiveInteger(separated_text[2])&&isPositiveInteger(separated_text[3])&&isPositiveInteger(separated_text[4])&&isPositiveInteger(separated_text[5])&&isPositiveInteger(separated_text[6])){
-						int sum = cube.query(Integer.parseInt(separated_text[1]), Integer.parseInt(separated_text[2]), Integer.parseInt(separated_text[3]), Integer.parseInt(separated_text[4]),Integer.parseInt(separated_text[5]),Integer.parseInt(separated_text[6]));
-						System.out.println("The sum is: "+ sum);
-					}
-				}else{
-					System.out.println("Too much or too little values.");
-				}
-			}else{
+			String command = separated_text[0];
+			
+			//Constrains
+			if (!command.equals("UPDATE") && !command.equals("QUERY")){
 				System.out.println("Cannot perform: "+ separated_text[0]);
+				continue;
+			}
+			if (separated_text.length < 5 || separated_text.length > 7 ){
+				System.out.println("Too much or too little values.");
+				continue;
+			}
+			String[] input_numbers = Arrays.copyOfRange(separated_text, 1,separated_text.length);
+			if(!arePositiveIntegers(input_numbers)){
+				System.out.println("The values are not positive numbers");
+				continue;
+			}
+			
+			int[] values = new int[input_numbers.length];
+			for(int i = 0; i < input_numbers.length; i++){
+				values[i] = Integer.parseInt(input_numbers[i]);
+			}
+			
+			try{
+				if(separated_text[0].contains("UPDATE") && values.length == 4){
+					cube.update(values);
+				}else if (separated_text[0].contains("QUERY") && values.length == 6){
+					int result = cube.query(values);
+					System.out.println("Query:" + result);
+				}else{
+					System.out.println("Too much or too little values.");
+				}
+			}catch(ArrayIndexOutOfBoundsException e){
+				System.out.println("Index out of bounds");
 			}
 		}
 
 	}
-	
+
 	/**
 	 * Method to check if a String is a positive Integer
+	 * 
 	 * @param s
 	 * @return True if the string is a positive Integer
 	 */
-	public static boolean isPositiveInteger(String s){
-		try{
+	private static boolean isPositiveInteger(String s) {
+		try {
 			int i = Integer.parseInt(s);
-			if(i > 0){
+			if (i > 0 && i < 100) {
 				return true;
-			}else{
+			} else {
 				return false;
 			}
-		}catch(NumberFormatException n){
+		} catch (NumberFormatException n) {
 			return false;
 		}
+	}
+
+	/**
+	 * Method to check if all records in the array are positive integers.
+	 * 
+	 * @param s_array
+	 *            A array of Strings
+	 * @return True if all records are positive integers. False if not.
+	 */
+	private static boolean arePositiveIntegers(String[] s_array) {
+		for (int i = 0; i < s_array.length; i++) {
+			if (!isPositiveInteger(s_array[i])) {
+				return false;
+			}
+		}
+		// All records were positive integers.
+		return true;
 	}
 
 }
